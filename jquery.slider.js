8280f52f93893d98f03cec9d5cb5c2eefde9d345
Daniel Dietrich <cafebab3@gmail.com>
2012-01-02 02:33:00 +0100

    temp commit on issue #1

--- jquery.slider.js
+++ jquery.slider.js
@@ -1,4 +1,6 @@
 /*
+ * author: Daniel Dietrich
+ *
  *  Example:
  *
  *  <script type='text/javascript' src='js/jquery.slider.js'></script>
@@ -22,61 +24,34 @@
  *  </script>
  */
 
-// self executing function (closure) that maps jQuery to the dollar sign
-// so it can't be overwritten by another library in the scope of its execution
 (function($) {
-  
-  // add a new function property to the jQuery.fn object
+
+  // called to init a slider container (e.g. div)
   $.fn.slider = function() {
-    
-    // there's no need to do $(this) because 'this' is already a jquery object
-    return this.each(function() {
-      
-      // create a jQuery object for each element 'this' passed to this closure
-      var obj = $(this)
-      var ul = $("ul", obj)
-      var li = ul.children()
-      var li_width = obj.width()
-      var ul_width = li_width * li.length
-
-      obj.css("overflow", "hidden")
-
-      ul.css({
-        "width": ul_width,
-        "margin": 0,
-        "padding": 0,
-        "list-style": "none",
-      })
-
-      li.css({
-        "width": li_width,
-        "float": "left",
-        "margin": 0,
-        "padding": 0,
-        "overflow": "hidden"
-      })
-
+    var that = this
+    that.attr("slide", 0)
+    $(window).resize(function(e) {
+      internal_resize(that)
     })
+    internal_init(that)
+    return internal_resize(that)
   }
 
   // slide back
-  // example: <a href="javascript:void(0)" onclick="$(this).slideBack()">back</a>
   $.fn.slideBack = function() {
     return this.each(function() {
       internal_slide(this, -1)
     })
   }
 
-  // slide next
-  // example: <a href="javascript:void(0)" onclick="$(this).slideNext()">next</a>
+  // next slide
   $.fn.slideNext = function() {
     return this.each(function() {
       internal_slide(this, 1)
     })
   }
 
-  // slide to specific index
-  // example: <a href="javascript:void(0)" onclick="$('#slider').slide(0)">first</a>
+  // goto slide, 0 <= index < slides.length
   $.fn.slide = function(index) {
     return this.each(function() {
       var obj = $(this)
@@ -85,11 +60,51 @@
         { "marginLeft": margin },
         "fast"
       )
+      obj.attr("slide", index)
     })
   }
 
-  // helper that slides back (dir = -1) or next (dir = 1)
-  // element is a child of current li
+  // set mandatory css attributes
+  function internal_init(that) {
+    return that.each(function() {
+      var obj = $(this)
+      var ul = $("ul", obj)
+      var li = ul.children()
+      var width = obj.width()
+      obj.css("overflow", "hidden")
+      ul.css({
+        "width": width * li.length,
+        "margin": 0,
+        "padding": 0,
+        "list-style": "none",
+      })
+      li.css({
+        "width": width,
+        "float": "left",
+        "margin": 0,
+        "padding": 0,
+        "overflow": "hidden"
+      })
+    })
+  }
+
+  // recalculate width (important case: scrollbar visibility changes)
+  function internal_resize(that) {
+    var index = that.attr("slide")
+    return that.each(function() {
+      var obj = $(this)
+      var ul = $("ul", obj)
+      var li = ul.children()
+      var width = obj.width()
+      ul.css({
+        "width": width * li.length,
+        "marginLeft": index * width * -1
+      })
+      li.css("width", width)
+    })
+  }
+
+  // slide next / back
   function internal_slide(element, dir) {
     return $(element).closest("li").each(function() {
       var li = $(this)
@@ -103,6 +118,7 @@
         { "marginLeft": margin },
         "fast"
       )
+      obj.attr("slide", index+dir)
     })
   }
 
